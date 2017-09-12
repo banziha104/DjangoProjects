33e060da03ac49c45bdc2f1374eab9e40f20e3ed
iyeongjun <they6687@naver.com>
2017-09-12 19:52:45 +0900

    Add SecondWeek

--- 3weeks/example1.py
+++ 3weeks/example1.py
@@ -1,5 +1,11 @@
 import requests
+from bs4 import BeautifulSoup
+url = "http://datalab.naver.com/keyword/sectionSearch.naver"
 
-url = ""
+r = requests.get(url)
 
-r = requests.get()
+if r.status_code == requests.codes.ok:
+    data = BeautifulSoup(r.text, "html.parser")
+    keywords = data.select(".sub_title .title")
+    for keyword in keywords:
+        print(keyword.text)
