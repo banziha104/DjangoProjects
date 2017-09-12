5e898599f59ad80c5cd7f92a857cf7b15410bc60
iyeongjun <they6687@naver.com>
2017-09-12 20:27:24 +0900

    Add 3Week

---
+++ 3weeks/example2.py
@@ -0,0 +1,15 @@
+import requests
+import json
+
+url = "http://datalab.naver.com/rc/rankList.naver"
+
+r = requests.post(url)
+
+if r.status_code == requests.codes.ok :
+    encoded_json = json.loads(r.text)
+    data = encoded_json["data"]
+    print(data)
+    for section in data:
+        print(section["name"])
+        print(section["items"])
+
